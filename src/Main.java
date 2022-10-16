073115d4aee9b62cd6f48bb2a5f269507b0ddfe6
shift-runstop <shift-runstop@protonmail.ch>
2022-10-16 11:58:17 +0100

    Multi-threaded server started, needs testing client

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,8 @@
 // mySQL JDBC GUI
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.*;
-import javax.swing.*;
 
-public class Main
-{
-    Client client = new Client();
-
-    public static String name="", email ="";
-    public static void main(String[] args){
-        JFrame f=new JFrame();
-        JLabel labell = new JLabel("Name: ");
-        //JLabel labe12 = new JLabel("Email: ");
-        JTextField textl = new JTextField(20);
-        //JTextField text2=new JTextField(20);
-        JButton b1=new JButton("login");
-        //JButton b2=new JButton("prev");
-        Connection con;
-        Statement st;
-        try{
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_create_DB", "root", "");
-            st=con.createStatement();
-            rs=st.executeQuery("select * from data");
-            if(rs.next()){
-                name=rs.getString("name");
-                //email=rs.getString("email");
-                textl.setText(name);
-                //text2.setText(email);
-            }
-        }catch(Exception e){}
-        JPanel p=new JPanel(new GridLayout(10,10));
-        p.add(labell);
-        p.add(textl);
-
-        f.add(p);
-        f.setVisible(true);
-        f.pack();
-
-        p.add(b1);
-
-        b1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                try {
-                    System.out.println("login pressed..");
-                    client.run();
-
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        });
+public class Main {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> new Server());
+        thread.start();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -13,43 +13,101 @@ import javax.swing.*;
 public class Server extends JFrame {
     JTextArea jta;
     public static ResultSet rs;
+    public boolean login = false;
 
-
+    public Server() {
+        jta = new JTextArea();
+        setLayout(new BorderLayout());
+        add(new JScrollPane(jta), BorderLayout.CENTER);
+        setTitle("Server");
+        setSize(500, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true); // It is necessary to show the frame here!
+        try {
+            // Create a server socket
+            ServerSocket serverSocket = new ServerSocket(8000);
+            jta.append("Server started at " + new Date() + '\n');
+            // Number a session
+            int sessionId = 1;
+            // Ready to create a session for every two players
+            while (true) {
+                jta.append("Waiting for players to join session " + '\n');
+                // Connect to player 1
+                Socket client1 = serverSocket.accept();
+                jta.append("Client 1 joined session " + '\n');
+                jta.append("Client 1's IP address" + client1.getInetAddress().getHostAddress() + '\n');
+                // Notify that the player is Player 1
+                new DataOutputStream(client1.getOutputStream()).writeInt(1);
+                // Connect to player 2
+                Socket client2 = serverSocket.accept();
+                jta.append("Client 2 joined session " + '\n');
+                jta.append("Client 2's IP address" + client2.getInetAddress().getHostAddress() + '\n');
+                // Notify that the player is Player 2
+                new DataOutputStream(client2.getOutputStream()).writeInt(2);
+                // Display this session and increment session number
+                jta.append("Start a thread for session " + sessionId++ + '\n');
+                // Find the client's host name, and IP address
+                InetAddress inetAddress = InetAddress.getLocalHost();
+                jta.append("Host name is " + inetAddress.getHostName() + '\n');
+                jta.append("IP Address is " + inetAddress.getHostAddress() + '\n');
+                // Create and start a new thread for this session of two players
+            }
+        }
+        catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
     private class ThreadClass extends Thread {
         Socket socket;
         private InetAddress address;
         private DataInputStream inputFromClient;
         private DataOutputStream outputToClient;           
-        
-        public ThreadClass(Socket socket){
+        public Statement st = null;
+        private int id = -1;
+        public ThreadClass(Socket socket) throws IOException{
+            try {
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_create_DB", "root", "root");
+                st = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+            }
             this.socket = socket;
             address = socket.getInetAddress();
-            try {
-                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
-                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
-            }catch (IOException e) {
-                System.err.println("Exception in class");
-                e.printStackTrace();
-            }
+            inputFromClient = new DataInputStream(socket.getInputStream());
+            outputToClient = new DataOutputStream(socket.getOutputStream());
         }
         
         public void run() {
             try {
-            while (true) {
-                
-                System.out.println("HELLO");
-            // Receive radius from the client
-            double radius = inputFromClient.readDouble();
-
-            // Compute area
-            double area = radius * radius * Math.PI;
-
-            // Send area back to the client
-            outputToClient.writeDouble(area);
-
-            jta.append("Radius received from client: " + radius + '\n');
-            jta.append("Area found: " + area + '\n');
-        }
+                while (login) {
+                    System.out.println("HELLO");
+                // Receive radius from the client
+                    double radius = inputFromClient.readDouble();
+                // Compute area
+                    double area = radius * radius * Math.PI;
+                // Send area back to the client
+                    outputToClient.writeDouble(area);
+                    jta.append("Radius received from client: " + radius + '\n');
+                    jta.append("Area found: " + area + '\n');
+                }
+                while (!login) {
+                    String username = inputFromClient.readUTF();
+                    String sql = "SELECT * FROM user WHERE username = '" + username + "'";
+                    String updateTOT = "\"update students set TOT_REQ=TOT_REQ+1 where STUD_ID='\" + id + \"'\"";
+                    rs = st.executeQuery(sql);
+                    if (rs.next()) {
+                        id = rs.getInt("STUD_ID");
+                        login = true;
+                        outputToClient.writeBoolean(true);
+                        outputToClient.writeUTF("Login successful: " + rs.getString("FNAME"));
+                        st.execute(updateTOT);
+                        System.out.println("Login successful");
+                    } else {
+                        outputToClient.writeBoolean(false);
+                        outputToClient.writeUTF("Login failed");
+                        System.out.println("Login failed");
+                    }
+                }
             } catch(Exception e) {
                 System.err.println(e + "on " + socket);
                 e.printStackTrace();
